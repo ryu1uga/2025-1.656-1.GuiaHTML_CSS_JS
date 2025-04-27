a6c86b1b8d9c867f2a60f90b28abf364aae01f4f
ryu1uga <20211953@aloe.ulima.edu.pe>
2025-04-27 01:43:11 -0500

    ejercicio 10 completado

--- ejercicio10/ejercicio10.js
+++ ejercicio10/ejercicio10.js
@@ -1,6 +1,58 @@
+let LimpiarBoton = function()
+{
+    let botones = document.getElementsByClassName("boton")
+    for (let boton of botones)
+    {
+        boton.innerText = ""
+    }
+}
+
+let TablaOpuesta = function(t)
+{
+    if (t == 1)
+    {
+        return 2
+    }
+    return 1
+}
+
 let SeleccionarBoton = function(t, x, y)
 {
-    
+    LimpiarBoton()
+    let botonSeleccionado1 = document.getElementById("t" + t + "_x" + x + "_y" + y)
+    botonSeleccionado1.innerText = "X"
+    let botonSeleccionado2 = document.getElementById("t" + TablaOpuesta(t) + "_x" + (9-x) + "_y" + (9-y))
+    botonSeleccionado2.innerText = "X"
+
+    if (x > 1)
+    {
+        let botonArriba1 = document.getElementById("t" + t + "_x" + (x-1) + "_y" + y)
+        botonArriba1.innerText = "X"
+        let botonArriba2 = document.getElementById("t" + TablaOpuesta(t) + "_x" + (10-x) + "_y" + (9-y))
+        botonArriba2.innerText = "X"
+    }
+    if (x < 8)
+    {
+        let botonAbajo1 = document.getElementById("t" + t + "_x" + (x+1) + "_y" + y)
+        botonAbajo1.innerText = "X"
+        let botonAbajo2 = document.getElementById("t" + TablaOpuesta(t) + "_x" + (8-x) + "_y" + (9-y))
+        botonAbajo2.innerText = "X"
+    }
+    if (y > 1)
+    {
+        let botonIzquierda1 = document.getElementById("t" + t + "_x" + x + "_y" + (y-1))
+        botonIzquierda1.innerText = "X"
+        let botonIzquierda2 = document.getElementById("t" + TablaOpuesta(t) + "_x" + (9-x) + "_y" + (10-y))
+        botonIzquierda2.innerText = "X"
+    }
+    if (y < 8)
+    {
+        let botonDerecha1 = document.getElementById("t" + t + "_x" + x + "_y" + (y+1))
+        botonDerecha1.innerText = "X"
+        let botonDerecha2 = document.getElementById("t" + TablaOpuesta(t) + "_x" + (9-x) + "_y" + (8-y))
+        botonDerecha2.innerText = "X"
+    }
+
 }
 
 let main = function()
@@ -9,10 +61,11 @@ let main = function()
     {
         for (let y = 1; y <= 8; y++)
         {
-            let botonIzquierda = document.getElementsById("t1 x" + x + " y" + y)
-            let botonDerecha = document.getElementsById("t2 x" + x + " y" + y)
+            let botonIzquierda = document.getElementById("t1_x" + x + "_y" + y)
+            let botonDerecha = document.getElementById("t2_x" + x + "_y" + y)
             
-            botonIzquierda.
+            botonIzquierda.addEventListener("click", function() {SeleccionarBoton(1, x, y)})
+            botonDerecha.addEventListener("click", function() {SeleccionarBoton(2, x, y)})
         }
     }
 }
